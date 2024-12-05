1c66a239c6ad5255c5cd96b5bb90b5b70109c0c3
rryuzakiii <itsamitkumar5393@gmail.com>
2024-12-05 19:29:21 +0530

    update

--- day5/day51.py
+++ day5/day51.py
@@ -1,68 +1,17 @@
-from collections import defaultdict, deque
 
-def parse(input):
-    rules, updates = input.split("\n\n")
-    rule_list = [tuple(map(int, line.split('|'))) for line in rules.splitlines()]
-    update_list = [list(map(int, line.split(','))) for line in updates.splitlines()]
-    return rule_list, update_list
-
-def build_rules(rule_list):
-    precedence = defaultdict(list)
-    for before, after in rule_list:
-        precedence[after].append(before)
-    return precedence
-
-def is_valid_update(update, pre):
-    index_map = {page: idx for idx, page in enumerate(update)}
-    for after, befores in pre.items():
-        if after in index_map:
-            after_idx = index_map[after]
-            for before in befores:
-                if before in index_map and index_map[before] > after_idx:
-                    return False
-    return True
-
-def topological_sort(update, pre):
-    graph = defaultdict(set)
-    in_degree = defaultdict(int)
-    for page in update:
-        graph[page] = set()
-        in_degree[page] = 0
-    for after, befores in pre.items():
-        if after in update:
-            for before in befores:
-                if before in update:
-                    graph[before].add(after)
-                    in_degree[after] += 1
-    queue = deque([node for node in update if in_degree[node] == 0])
-    sorted_list = []
-    while queue:
-        node = queue.popleft()
-        sorted_list.append(node)
-        for neighbor in graph[node]:
-            in_degree[neighbor] -= 1
-            if in_degree[neighbor] == 0:
-                queue.append(neighbor)
-    return sorted_list
-
-def find(update):
-    n = len(update)
-    return update[n // 2]
-
-def solve(file_path):
-    with open(file_path, 'r') as file:
-        input = file.read()
-    rule_list, update_list = parse(input)
-    pre = build_rules(rule_list)
-    total_middle_sum = 0
-
-    for update in update_list:
-        if not is_valid_update(update, pre):
-            corrected_update = topological_sort(update, pre)
-            total_middle_sum += find(corrected_update)
-    
-    return total_middle_sum
-
-file_path = "input.txt"  
-result = solve(file_path)
-print(result)
+from itertools import combinations
+data = [_.strip() for _ in open("input.txt", "r").readlines()]
+rules, updates = [[int(k) for k in _.split("|")] for _ in data[0:data.index('')]], [[int(k) for k in _.split(",")] for _ in data[data.index('') + 1:]]
+correct = []
+for update in updates:
+    if all(list(_) in rules for _ in combinations(update, 2)):
+        correct.append(update)
+wrong = [_ for _ in updates if _ not in correct]
+s2 = 0
+for update in wrong:
+    while not all(list(_) in rules for _ in combinations(update, 2)):
+        for i in range(len(update) - 1):
+            if [update[i], update[i + 1]] not in rules:
+                update[i], update[i + 1] = update[i + 1], update[i]
+    s2 += update[len(update) // 2]
+print(s2)
